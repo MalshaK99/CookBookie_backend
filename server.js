efcb6b801e3f349163d25150546f5c6045a78ee4
Malsha Karunarathna <cst20060@std.uwu.ac.lk>
2024-11-09 09:58:53 +0530

    up

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const cors = require('cors');  
 const connectDB = require('./config/db');  
 const userRoutes = require('./routes/userRoutes');
-const reviewRoutes=require('./routes/reviewRoutes')  
-const recipeRoutes=require('./routes/recipeRoutes')
+const reviewRoutes = require('./routes/reviewRoutes');  
+const recipeRoutes = require('./routes/recipeRoutes');
+const emailRoutes = require('./routes/emailRoutes'); // Import the email route
 const app = express();  
 const path = require("path");
 require('dotenv').config();  
@@ -14,8 +15,9 @@ app.use(cors());
 app.use(express.json());  
 
 app.use('/api/users', userRoutes); 
-app.use('/api/reviews',reviewRoutes); 
-app.use('/api/recipes',recipeRoutes);
+app.use('/api/reviews', reviewRoutes); 
+app.use('/api/recipes', recipeRoutes);
+app.use('/api', emailRoutes); // Add the email route here
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;  
@@ -23,4 +25,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {  
     console.log(`Server is running on port ${PORT}`);  
 });
-
